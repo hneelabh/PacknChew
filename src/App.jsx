4b073c70f7f6a79c7c71c3636c72d7a44b2b3ab5
hneelabh <hneelabh13@gmail.com>
2024-06-12 17:08:12 +0530

    Scroll issue resolved

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,75 @@
+// import React, { useEffect } from "react";
+// import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import BrowserRouter, Routes, and Route
+// import AOS from 'aos';
+// import 'aos/dist/aos.css';
+
+// import Navbar from "./components/Navbar";
+// import Home from "./components/Home";
+// import Footer from "./components/Footer";
+// import Services from "./components/Services";
+// import WhyChooseUs from "./components/WhyChooseUs";
+// import ChefsNote from "./components/ChefsNote";
+
+// import Menu from "./components/Menu";
+// import Reviews from "./components/Review";
+// import Review from "./components/Review";
+// import Reservation from "./components/Reservation";
+// import Awards from "./components/Awards";
+
+// const App = () => {
+
+//   //AOS Init
+//   useEffect(() => {
+//     AOS.init({
+//       offest: 100,
+//       duration: 800,
+//       easing: "ease-in-sine",
+//       delay: 100,
+//     });
+//     AOS.refresh();
+//   }, []);
+  
+
+//   return (
+
+//     <Router>
+//     <div>
+
+//       <Routes>
+//         {/* <Route path="/login" element={<Login />} />
+//         <Route path="/bookBattery" element={<BookBattery />} /> */}
+
+//         <Route path="/" element={<>
+//           <Navbar/>
+//           <Home/>
+//           <Services/>
+//           <Footer />
+//         </>} />
+
+//         <Route path="/reserve" element={<>
+//           <Navbar/>
+//           <Reservation />
+//           <Footer />
+//         </>} />
+
+
+//         <Route path="/about" element={<>
+//           <Navbar/>
+//           <ChefsNote />
+//           <Awards />
+//           <WhyChooseUs />
+//           <Footer />
+//         </>} />
+
+//       </Routes>
+
+//     </div>
+//   </Router>
+//   );
+// };
+
+// export default App;
+
 import React, { useEffect } from "react";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; // Import BrowserRouter, Routes, and Route
 import AOS from 'aos';
@@ -16,9 +88,11 @@ import Review from "./components/Review";
 import Reservation from "./components/Reservation";
 import Awards from "./components/Awards";
 
+import ScrollToTop from './components/ScrollToTop'; // Import the ScrollToTop component
+
 const App = () => {
 
-  //AOS Init
+  // AOS Init
   useEffect(() => {
     AOS.init({
       offest: 100,
@@ -28,43 +102,41 @@ const App = () => {
     });
     AOS.refresh();
   }, []);
-  
 
   return (
-
     <Router>
-    <div>
+      <ScrollToTop /> {/* Include ScrollToTop component here */}
+      <div>
+        <Routes>
+          <Route path="/" element={
+            <>
+              <Navbar />
+              <Home />
+              <Services />
+              <Footer />
+            </>
+          } />
 
-      <Routes>
-        {/* <Route path="/login" element={<Login />} />
-        <Route path="/bookBattery" element={<BookBattery />} /> */}
+          <Route path="/reserve" element={
+            <>
+              <Navbar />
+              <Reservation />
+              <Footer />
+            </>
+          } />
 
-        <Route path="/" element={<>
-          <Navbar/>
-          <Home/>
-          <Services/>
-          <Footer />
-        </>} />
-
-        <Route path="/reserve" element={<>
-          <Navbar/>
-          <Reservation />
-          <Footer />
-        </>} />
-
-
-        <Route path="/about" element={<>
-          <Navbar/>
-          <ChefsNote />
-          <Awards />
-          <WhyChooseUs />
-          <Footer />
-        </>} />
-
-      </Routes>
-
-    </div>
-  </Router>
+          <Route path="/about" element={
+            <>
+              <Navbar />
+              <ChefsNote />
+              <Awards />
+              <WhyChooseUs />
+              <Footer />
+            </>
+          } />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
